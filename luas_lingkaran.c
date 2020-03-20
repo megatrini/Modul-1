1d64db661f9df91b84a24f3796157373ca7ae888
Mega Trini Oktaviani <62134888+megatrini@users.noreply.github.com>
2020-03-20 09:54:43 +0700

    Create luas_lingkaran.c

---
+++ luas_lingkaran.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{ int r;
+float phi, luas;
+phi = 3.14;
+printf ("Masukan jari-jari lingkaran(cm) :", r);
+scanf ("%i, &r");
+luas = phi*r*r;
+printf("Jadi luas lingkaran tersebut adalah :%f\n",luas);
+system ("pause");
+return 0;
+}
